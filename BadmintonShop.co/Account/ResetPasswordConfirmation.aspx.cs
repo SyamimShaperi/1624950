f3716a53017606510113ddc04b5b8e2fe5844c4e
SyamimShaperi <36612715+SyamimShaperi@users.noreply.github.com>
2018-02-26 12:12:56 +0800

    Add project files.

---
+++ BadmintonShop.co/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace BadmintonShop.co.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ BadmintonShop.co/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BadmintonShop.co.Global" Language="C#" %>
